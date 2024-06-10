a2b735f456da9e99274dd1819a7945dcce8c69b4
Max Krivonos <mkriwonos68@gmail.com>
2024-06-10 21:59:24 +0300

    module6-task2

---
+++ src/res/strings.ts
@@ -0,0 +1,8 @@
+import { SortMethod } from '../types/sort-method';
+
+export const SortMethodTexts = {
+  [SortMethod.POPULAR]: 'Popular',
+  [SortMethod.PRICE_HIGH_TO_LOW]: 'Price: high to low',
+  [SortMethod.PRICE_LOW_TO_HIGH]: 'Price: low to high',
+  [SortMethod.TOP_RATED]: 'Top rated first'
+};
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -2,3 +2,4 @@ import { State } from '../types/state';
 
 export const selectCurrentCity = (state: State) => state.city;
 export const selectCurrentOffers = (state: State) => state.offers;
+export const selectCurrentSortMethod = (state: State) => state.sortMethod;
